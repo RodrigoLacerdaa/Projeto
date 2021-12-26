5ccb7be3738f866117734624e721e237bb6c4902
RodrigoLacerdaa <81272703+RodrigoLacerdaa@users.noreply.github.com>
2021-12-26 17:05:34 -0300

    ml

--- projeto/src/projeto/projetoX.java
+++ projeto/src/projeto/projetoX.java
@@ -7,7 +7,7 @@ public class projetoX {
 		int criancaIdade = 6;
 		int jovemidade = 18;
 		int idosoIdade = 64;
-		
+		dasd
 		adsasdasd
 		
 		System.out.println("qual e a idade da " + criancaIdade);	
